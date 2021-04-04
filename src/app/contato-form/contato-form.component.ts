2f46a1376665c6e9bd359baf01ff1e165caed1b4
Angélica <angelicarocha67@yahoo.com.br>
2021-04-04 12:21:27 -0300

    Commit inicial

---
+++ src/app/contato-form/contato-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Contato } from '../models/contato.model';
+import { ContatoService } from '../services/contato.service';
+
+@Component({
+  selector: 'app-contato-form',
+  templateUrl: './contato-form.component.html',
+  styleUrls: ['./contato-form.component.css']
+})
+
+export class ContatoFormComponent implements OnInit {
+
+  @Input() contato: Contato;
+  @Output() saveContato = new EventEmitter();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form: NgForm){
+    this.saveContato.emit(form)
+  }
+
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Contato } from '../models/contato.model';
+import { ContatoService } from '../services/contato.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  contato: Contato;
+  contatos: Contato[];
+
+  constructor(private contatoService: ContatoService) { }
+
+  ngOnInit() {
+    this.contato = new Contato();
+    this.contatos = this.contatoService.getAll();
+  }
+
+  saveContato(form: NgForm) {
+    this.contato = form.value;
+    this.contato.id = new Date().getTime().toString();
+    this.contatoService.save(this.contato);
+    this.contatos.push(this.contato);
+    form.resetForm();
+  }
+
+}
